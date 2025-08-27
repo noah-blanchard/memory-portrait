61c8137710dcfe291a3030a6a2415bc33abd1d23
Noah Blanchard <matanaworgen@gmail.com>
2025-08-26 23:29:36 -0400

    view port fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'Memory Portrait',
   description: 'Memory Portrait Booking App',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  // viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+};  
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
